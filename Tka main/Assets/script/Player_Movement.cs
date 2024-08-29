61e9702d34ed3e323dc9ad26c7e99d037fadebc8
Monob <dlgustj0430@gmail.com>
2024-08-29 10:39:42 +0900

    sfd

--- Tka main/Assets/script/Player_Movement.cs
+++ Tka main/Assets/script/Player_Movement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UIElements;
 
 public class Player_Movement : MonoBehaviour
@@ -17,11 +18,16 @@ public class Player_Movement : MonoBehaviour
     // public Transform groundCheck; // 바닥 체크 위치
 
     public bool isGround = false; // 플레이어가 바닥에 있는지 여부
+    public bool is_attacking = false; // 플레이어가 공격 중인지 여부
     
     Transform tr;
     // Collider2D col;
     Rigidbody2D rb; // 플레이어의 Rigidbody2D 컴포넌트
     SpriteRenderer spriteRenderer;
+    public Camera cam;
+
+    public PostProcessVolume volume;
+
 
     void Awake()
     {
@@ -40,28 +46,14 @@ public class Player_Movement : MonoBehaviour
 
     void Update()
     {
-        if(transform.position.y<-50){
-            transform.position = new Vector2(0,0);
-        }
-        if(Mathf.Abs(rb.velocity.y)<0.0001f)    isGround = true;
-
-        // 플레이어에게 가해지는 마찰력을 계산합니다.
-        Vector2 frictionForce = new Vector2(-rb.velocity.x * friction, 0);
-        rb.AddForce(frictionForce, ForceMode2D.Force);
-        
-        Vector2 clampedVelocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), rb.velocity.y);
-        rb.velocity = clampedVelocity;
-        int dir = rb.velocity.x > 0?1:rb.velocity.x<0?-1:0;
-        if(dir!=0)
+        if(Mathf.Abs(rb.velocity.x)>0.1f)
         {
-            transform.localScale = new Vector3(dir, 1, 1);
+            Run.SetBool("Run", true);
         }
-        float horizontalInput = Input.GetAxis("Horizontal"); // 수평 입력 값
-        Vector2 moveDirection = new Vector2(horizontalInput, 0); // 이동 방향 벡터
-
-
-    // 플레이어에게 이동 힘을 가합니다.
-        rb.AddForce(moveDirection * speed);
+        else
+        {
+            Run.SetBool("Run", false);
+        }
 
 
         if (Input.GetKeyDown(KeyCode.Space))
@@ -73,19 +65,93 @@ public class Player_Movement : MonoBehaviour
             }
         }
 
+
         Attack();
+        if(is_attacking == true)   return;
+        Move();
+    }
+    void Move()
+    {
+        if(transform.position.y<-50){
+            transform.position = new Vector2(0,0);
+        }
+        if(Mathf.Abs(rb.velocity.y)<0.0001f)    isGround = true;
+
         
+        Vector2 clampedVelocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), rb.velocity.y);
+        rb.velocity = clampedVelocity;
+        int dir = rb.velocity.x > 0?1:rb.velocity.x<0?-1:0;
+        if(dir!=0)
+        {
+            transform.localScale = new Vector3(dir, 1, 1);
+        }
+        float horizontalInput = Input.GetAxis("Horizontal"); // 수평 입력 값
+        Vector2 moveDirection = new Vector2(horizontalInput, 0); // 이동 방향 벡터
+                
+        // 플레이어에게 가해지는 마찰력을 계산합니다.
+        Vector2 frictionForce = new Vector2(-rb.velocity.x * friction, 0);
+        rb.AddForce(frictionForce, ForceMode2D.Force);
+
+
+        // 플레이어에게 이동 힘을 가합니다.
+        rb.AddForce(moveDirection * speed);
+
     }
 
     void Attack()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            Run.SetBool("Run", true);
+            if(is_attacking == false)
+            {
+                is_attacking = true;
+                Time.timeScale = 0.6f;
+                StopCoroutine(Post_Process_change(volume, 0.1f));
+                volume.weight = 0;
+                StartCoroutine(Post_Process_change(volume, 0.1f));
+                Run.SetBool("Attack", true);
+                StartCoroutine(AttackCoroutine());
+            }
         }
-        else if(Input.GetMouseButtonUp(0))
+    }
+    IEnumerator Post_Process_change(PostProcessVolume volume1, float time)
+    {
+        float t = 0;
+        while(volume1.weight < 0.9f)
         {
-            Run.SetBool("Run", false);
+            t += Time.deltaTime;
+            volume1.weight = Mathf.Lerp(volume1.weight, 1, Time.deltaTime*6);
+            yield return new WaitForFixedUpdate();
+        }
+        while(volume1.weight > 0f)
+        {
+            t += Time.deltaTime;
+            volume1.weight = Mathf.Lerp(volume1.weight, -0.1f, Time.deltaTime*6);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+    IEnumerator AttackCoroutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        // 플레이어에게 가해지는 마찰력을 계산합니다.
+        Vector2 frictionForce = new Vector2(-rb.velocity.x * friction*2, 0);
+        rb.AddForce(frictionForce, ForceMode2D.Force);
+        // rb.velocity = Vector2.zero;
+        rb.AddForce(new Vector2(transform.localScale.x * 3, 0), ForceMode2D.Impulse);
+        StartCoroutine(Camera_Shake(4, 0.2f));
+        yield return new WaitForSeconds(0.25f);
+        is_attacking = false;
+        Run.SetBool("Attack", false);
+        Time.timeScale = 1f;
+    }
+
+    IEnumerator Camera_Shake(int cnt, float time, float power = 0.3f)
+    {
+        Vector3 initialPosition = cam.transform.position;
+        for(int i=0; i<cnt; i++)
+        {
+            cam.transform.position =Random.insideUnitSphere*power + initialPosition;
+            yield return new WaitForSeconds(time/cnt);
         }
     }
 
