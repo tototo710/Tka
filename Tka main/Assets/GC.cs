2a013133c81c1ce7f4d9a1cebeae87c9feba7b8e
Monob <dlgustj0430@gmail.com>
2024-09-10 09:54:07 +0900

    ㅇㄴ

--- Tka main/Assets/GC.cs
+++ Tka main/Assets/GC.cs
@@ -7,21 +7,12 @@ public class GC : MonoBehaviour
     public bool isGround = true; // 플레이어가 바닥에 있는지 여부
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.gameObject.layer == 6)
-        {
-            isGround = true;
-        }
+        isGround = true;
     }
     private void OnTriggerStay2D(Collider2D other) {
-        if(other.gameObject.layer == 6)
-        {
-            isGround = true;
-        }
+        isGround = true;
     }
     private void OnTriggerExit2D(Collider2D other) {
-        if(other.gameObject.layer == 6)
-        {
-            isGround = false;
-        }
+        isGround = false;
     }
 }
